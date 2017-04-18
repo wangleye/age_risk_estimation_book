ce0d8de318c195846f1f229cf03eebfd6a9df169
Leye <wangleye@gmail.com>
2017-04-19 00:27:50 +0800

    fix some pylint issues

--- training_data/clear_training_data.py
+++ training_data/clear_training_data.py
@@ -1,12 +1,17 @@
+"""
+remove the users without any reading records
+"""
 def has_indication(line):
+    """
+    decide whether a user has any reading indication
+    """
     words = line.split(' ')
     sum_ind = 0
     for i in range(1, len(words) - 1):
         sum_ind += float(words[i])
     if sum_ind == 0.0:
         return False
-    else:
-        return True
+    return True
 
 
 if __name__ == "__main__":
